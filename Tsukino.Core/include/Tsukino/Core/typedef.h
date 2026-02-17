9066cc02f4b9e08072dfda708212f34f7cdaa44c
GALLERIA\maihe <maihei541@gmail.com>
2026-02-17 19:01:14 +0900

    [Add] Common integer type definitions in Core

---
+++ Tsukino.Core/include/Tsukino/Core/typedef.h
@@ -0,0 +1,19 @@
+//--------------------------------------------------------------
+//! @file	typedef.h
+//! @brief  ‹¤’ÊŒ^’è‹`
+//! @author Rú±ˆ¤
+//--------------------------------------------------------------
+#pragma once
+#include <cstdint>
+
+// •„†•t‚«®”
+using i8  = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+
+// •„†‚È‚µ®”
+using u8  = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
