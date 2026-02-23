ac035f1d8ab4919e24372b2bb107c6a6799c391f
RX16\tamami197508 <maihei541@gmail.com>
2026-02-23 21:52:29 +0900

    [Add] Entity type alias for ECS core

---
+++ Tsukino.Core/include/Tsukino/Core/ECS/Entity.hpp
@@ -0,0 +1,11 @@
+//--------------------------------------------------------------------
+// @file   Entity.hpp
+// @brief  エンティティラッパーの定義
+// @author 山﨑愛
+//--------------------------------------------------------------------
+#pragma once
+#include <entt/entt.hpp>
+// 名前空間 : Tsukino::ECS
+namespace Tsukino::ECS {
+    using Entity = entt::entity;
+}    // namespace Tsukino::ECS
