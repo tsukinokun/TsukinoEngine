e2e1ce06104e9711f8569fb37766d6f1c38f84e3
RX16\tamami197508 <maihei541@gmail.com>
2026-02-27 03:45:49 +0900

    [Implement] basic AssetManager operations
    - Define static members in AssetManager.cpp
    - Add Exists() and Get() implementations

---
+++ Tsukino.Engine/src/Asset/AssetManager.cpp
@@ -0,0 +1,29 @@
+//--------------------------------------------------------------
+//! @file   AssetManager.cpp
+//! @brief  アセット管理クラスの実装
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#include <Tsukino/Engine/Asset/AssetManager.hpp>
+#include <Tsukino/Engine/Asset/AssetMap.hpp>
+// 名前空間 : Tsukino::Asset
+namespace Tsukino::Asset {
+    // staticメンバ変数の実体
+    AssetMap                       AssetManager::s_Assets;
+    std::vector<Tsukino::Core::Ref<IAssetLoader>> AssetManager::s_Loaders;
+
+    //--------------------------------------------------------------
+    //! @brief アセットハンドルが存在するか確認する関数
+    //--------------------------------------------------------------
+    bool AssetManager::Exists(AssetHandle handle) {
+        return s_Assets.contains(handle.Value());
+    }
+
+    //--------------------------------------------------------------
+    //! @brief 任意のアセットハンドルからアセットを取得する関数
+    //--------------------------------------------------------------
+    Tsukino::Core::Ref<IAsset> AssetManager::Get(AssetHandle handle) {
+        auto it = s_Assets.find(handle.Value());
+        return it != s_Assets.end() ? it->second : nullptr;
+    }
+
+}    // namespace Tsukino::Asset
