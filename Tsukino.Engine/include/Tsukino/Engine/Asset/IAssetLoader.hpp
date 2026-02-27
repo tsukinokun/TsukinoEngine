ff1d06fbe7869c0e02dc346a71e741059e91438d
RX16\tamami197508 <maihei541@gmail.com>
2026-02-27 10:27:13 +0900

    [Add] IAssetLoader interface
    - Define CanLoad() and Load() virtual functions
    - Establish loader abstraction for AssetManager

---
+++ Tsukino.Engine/include/Tsukino/Engine/Asset/IAssetLoader.hpp
@@ -0,0 +1,35 @@
+//--------------------------------------------------------------
+//! @file   IAssetLoader.hpp
+//! @brief  アセットローダーのインターフェース
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include <Tsukino/Core/Path.hpp>
+#include <Tsukino/Core/Memory.hpp>
+#include <Tsukino/Engine/Asset/IAsset.hpp>
+// namespace Tsukino::Asset
+namespace Tsukino::Asset {
+    //--------------------------------------------------------------
+    //! @class  IAssetLoader
+    //! @brief  アセットをロードするための抽象インターフェース
+    //--------------------------------------------------------------
+    class IAssetLoader {
+    public:
+        virtual ~IAssetLoader() = default;
+
+        //--------------------------------------------------------------
+        // このローダーが指定拡張子を扱えるか判定する
+        //! @param extension [in] ".png" や ".shader" などの拡張子
+        //! @return 対応している場合 true
+        //--------------------------------------------------------------
+        virtual bool CanLoad(const std::string& extension) const = 0;
+
+        //--------------------------------------------------------------
+        // アセットをロードする
+        //! @param path [in] ロード対象のパス
+        //! @return ロードされたアセット（失敗時は nullptr）
+        //--------------------------------------------------------------
+        virtual Tsukino::Core::Ref<IAsset> Load(const Tsukino::Core::Path& path) = 0;
+    };
+
+}    // namespace Tsukino::Asset
