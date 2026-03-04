7ca7cfb0a7558f54b7a300315ee5dc229453c659
RX16\tamami197508 <maihei541@gmail.com>
2026-03-04 23:55:38 +0900

    [Add] AssetImporter framework

---
+++ Tsukino.Engine/src/Asset/Importer/TextureImporter.cpp
@@ -0,0 +1,16 @@
+﻿//--------------------------------------------------------------
+//! @file   TextureImporter.cpp
+//! @brief  テクスチャのインポータークラスの実装
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#include <Tsukino/Engine/Asset/Import/TextureImporter.hpp>
+// 名前空間 : Tsukino::Asset
+namespace Tsukino::Asset {
+    //--------------------------------------------------------------
+    //! @brief  テクスチャアセットをインポートする関数
+    //--------------------------------------------------------------
+    Tsukino::Core::Ref<IAsset> TextureImporter::Import(const Tsukino::Core::Path& path) {
+        // 実装は次のブランチで行う
+        return nullptr;
+    }
+}    // namespace Tsukino::Asset
