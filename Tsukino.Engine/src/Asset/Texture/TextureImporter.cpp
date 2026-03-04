c8e3460fe66262725ad8c60f315327e873077bbf
RX16\tamami197508 <maihei541@gmail.com>
2026-03-05 02:35:49 +0900

    [Add] DirectXTex as external static library

--- Tsukino.Engine/src/Asset/Texture/TextureImporter.cpp
+++ Tsukino.Engine/src/Asset/Texture/TextureImporter.cpp
@@ -4,6 +4,9 @@
 //! @author 山﨑愛
 //--------------------------------------------------------------
 #include <Tsukino/Engine/Asset/Texture/TextureImporter.hpp>
+
+#include <DirectXTex/DirectXTex.h>
+
 // 名前空間 : Tsukino::Asset
 namespace Tsukino::Asset {
     //--------------------------------------------------------------
