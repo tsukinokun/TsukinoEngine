59a71e03dfdd9897948bf1f35aaf1b57ff77f8f8
RX16\tamami197508 <maihei541@gmail.com>
2026-03-14 00:35:01 +0900

    [Add] vertex pnuv

---
+++ Tsukino.Renderer/include/Tsukino/Renderer/Vertex/VertexPNUV.hpp
@@ -0,0 +1,19 @@
+﻿//--------------------------------------------------------------
+//! @file   VertexPNUV.hpp
+//! @brief  頂点構造体（位置 + 法線 + UV）
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include <DirectXMath.h>
+// 名前空間 : Tsukino::Renderer
+namespace Tsukino::Renderer {
+    //--------------------------------------------------------------
+    //! @struct VertexPNUV
+    //! @brief  頂点構造体（位置 + 法線 + UV）
+    //--------------------------------------------------------------
+    struct VertexPNUV {
+        DirectX::XMFLOAT3 position;    // 頂点の位置（x, y, z）
+        DirectX::XMFLOAT3 normal;      // 頂点の法線ベクトル（x, y, z）
+        DirectX::XMFLOAT2 uv;          // 頂点のUV座標（u, v）
+    };
+}    // namespace Tsukino::Renderer
