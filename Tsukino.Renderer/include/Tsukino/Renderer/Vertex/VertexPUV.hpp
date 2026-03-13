2e3c682823fd592d2b5e5f016da6730b61f7e61d
RX16\tamami197508 <maihei541@gmail.com>
2026-03-14 00:23:11 +0900

    [Add] vertex puv

---
+++ Tsukino.Renderer/include/Tsukino/Renderer/Vertex/VertexPUV.hpp
@@ -0,0 +1,18 @@
+﻿//--------------------------------------------------------------
+//! @file   VertexPUV.hpp
+//! @brief  頂点構造体（位置 + UV）
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include <DirectXMath.h>
+// 名前空間 : Tsukino::Renderer
+namespace Tsukino::Renderer {
+    //--------------------------------------------------------------
+    //! @struct VertexPUV
+    //! @brief  頂点構造体（位置 + UV）
+    //--------------------------------------------------------------
+    struct VertexPUV {
+        DirectX::XMFLOAT3 position;    // 頂点の位置（x, y, z）
+        DirectX::XMFLOAT2 uv;          // 頂点のUV座標（u, v）
+    };
+}    // namespace Tsukino::Renderer
