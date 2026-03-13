add9cea08b7952a1dfdc344db6bc9802522e3e43
RX16\tamami197508 <maihei541@gmail.com>
2026-03-14 00:04:18 +0900

    [Add] vertex format enum

---
+++ Tsukino.Renderer/include/Tsukino/Renderer/Vertex/VertexFormat.hpp
@@ -0,0 +1,21 @@
+﻿//--------------------------------------------------------------
+//! @file       VertexFormat.hpp
+//! @brief      GPU に渡す頂点フォーマットの種類を定義
+//! @author     山﨑愛
+//--------------------------------------------------------------
+#pragma once
+// 名前空間 : Tsukino::Renderer
+namespace Tsukino::Renderer {
+    //--------------------------------------------------------------
+    //! @enum  VertexFormat
+    //! @brief 頂点フォーマットの種類を定義する列挙型
+    //--------------------------------------------------------------
+    enum class VertexFormat {
+        Unknown = 0,         // 不明なフォーマット
+        PositionUV,          // 2D / Sprite / UI
+        PositionNormalUV,    // 通常の 3D Mesh
+        Skinned,             // スキニング（アニメーション）
+        Particle,            // パーティクルなど
+    };
+
+}    // namespace Tsukino::Renderer
