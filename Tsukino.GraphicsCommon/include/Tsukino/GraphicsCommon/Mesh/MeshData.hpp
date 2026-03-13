3d4a9be352e9a4849f40585dc9e17d682079cc7d
RX16\tamami197508 <maihei541@gmail.com>
2026-03-14 03:03:17 +0900

    [Add] mesh data struct

---
+++ Tsukino.GraphicsCommon/include/Tsukino/GraphicsCommon/Mesh/MeshData.hpp
@@ -0,0 +1,30 @@
+﻿//--------------------------------------------------------------
+//! @file   MeshData.hpp
+//! @brief  メッシュデータの構造体を定義
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include <Tsukino/GraphicsCommon/Vertex/VertexFormat.hpp>
+
+#include <Tsukino/Core/typedef.hpp>
+
+#include <vector>
+#include <cstdint>
+// 名前空間 Tsukino::GraphicsCommon
+namespace Tsukino::GraphicsCommon {
+    //--------------------------------------------------------------
+    //! @struct MeshData
+    //! @brief  メッシュデータの構造体
+    //--------------------------------------------------------------
+    struct MeshData {
+        std::vector<u8>  vertexData;    // 生の頂点バイト列
+        std::vector<u32> indices;       // 頂点インデックス
+
+        u32 vertexStride = 0;    // 頂点データの1頂点あたりのバイト数
+        u32 vertexCount  = 0;    // 頂点の総数
+        u32 indexCount   = 0;    // 頂点インデックスの総数
+
+        VertexFormat format = VertexFormat::Unknown;    // 頂点のフォーマット
+    };
+
+}    // namespace Tsukino::GraphicsCommon
