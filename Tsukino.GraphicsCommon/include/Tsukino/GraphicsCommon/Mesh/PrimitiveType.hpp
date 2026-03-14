d7a6388823a65f40ec4269c7581a94f88e019b8d
RX16\tamami197508 <maihei541@gmail.com>
2026-03-15 00:53:32 +0900

    [Add] primitive type enum class

---
+++ Tsukino.GraphicsCommon/include/Tsukino/GraphicsCommon/Mesh/PrimitiveType.hpp
@@ -0,0 +1,22 @@
+﻿//--------------------------------------------------------------
+//! @file   PrimitiveType.hpp
+//! @brief  メッシュのプリミティブデータを生成する関数の宣言
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include "MeshData.hpp"
+// 名前空間 Tsukino::GraphicsCommon
+namespace Tsukino::GraphicsCommon {
+    //--------------------------------------------------------------
+    //! @enum class PrimitiveType
+    //! @brief  プリミティブの種類を表す列挙型
+    //--------------------------------------------------------------
+    enum class PrimitiveType {
+        Quad,              // 四角形
+        FullscreenQuad,    // 全画面四角形
+        Cube,              // 立方体
+        Sphere,            // 球
+        Count              // 列挙型の要素数を表す
+    };
+
+}    // namespace Tsukino::GraphicsCommon
