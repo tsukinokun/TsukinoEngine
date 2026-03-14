cea595db60632da847a0d3f4ac30a3e120257c30
RX16\tamami197508 <maihei541@gmail.com>
2026-03-15 00:32:24 +0900

    [Add] create quad mesh function

---
+++ Tsukino.GraphicsCommon/include/Tsukino/GraphicsCommon/Mesh/MeshPrimitives.hpp
@@ -0,0 +1,17 @@
+﻿//--------------------------------------------------------------
+//! @file   MeshPrimitives.hpp
+//! @brief  メッシュのプリミティブデータを生成する関数の宣言
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include "MeshData.hpp"
+// 名前空間 Tsukino::GraphicsCommon
+namespace Tsukino::GraphicsCommon {
+    //--------------------------------------------------------------
+    // 四角形のメッシュデータを生成する関数
+    //! @return 四角形のメッシュデータ
+    //--------------------------------------------------------------
+    [[nodiscard]]
+    MeshData CreateQuadMeshData();
+
+}    // namespace Tsukino::GraphicsCommon
---
+++ Tsukino.GraphicsCommon/src/Mesh/MeshPrimitives.cpp
@@ -0,0 +1,44 @@
+﻿//--------------------------------------------------------------
+//! @file   MeshPrimitives.cpp
+//! @brief  メッシュのプリミティブデータを生成する関数の定
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#include <Tsukino/GraphicsCommon/Mesh/MeshPrimitives.hpp>
+#include <Tsukino/GraphicsCommon/Vertex/VertexFormat.hpp>
+#include <Tsukino/GraphicsCommon/Vertex/VertexPUV.hpp>
+// 名前空間 Tsukino::GraphicsCommon
+namespace Tsukino::GraphicsCommon {
+    //--------------------------------------------------------------
+    //! @brief 四角形のメッシュデータを生成する関数
+    //--------------------------------------------------------------
+    MeshData CreateQuadMeshData() {
+        MeshData mesh;    // メッシュデータ構造体のインスタンスを作成
+
+        // 頂点フォーマットを指定
+        mesh.format = VertexFormat::PositionUV;
+
+        // 1頂点のサイズ
+        mesh.vertexStride = sizeof(VertexPUV);
+
+        // Quad の頂点データ
+        VertexPUV vertices[4] = {
+            {{-0.5f, -0.5f, 0.0f}, {0.0f, 1.0f}},
+            {{0.5f, -0.5f, 0.0f},  {1.0f, 1.0f}},
+            {{0.5f, 0.5f, 0.0f},   {1.0f, 0.0f}},
+            {{-0.5f, 0.5f, 0.0f},  {0.0f, 0.0f}},
+        };
+
+        mesh.vertexCount = 4;
+
+        // 生バイト列としてコピー
+        mesh.vertexData.resize(sizeof(vertices));
+        std::memcpy(mesh.vertexData.data(), vertices, sizeof(vertices));
+
+        // インデックス
+        mesh.indices    = {0, 1, 2, 2, 3, 0};
+        mesh.indexCount = 6;
+
+        return mesh;
+    }
+
+}    // namespace Tsukino::GraphicsCommon
