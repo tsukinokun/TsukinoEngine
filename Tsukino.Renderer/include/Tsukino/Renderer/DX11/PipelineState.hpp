32f75f0b05cfd445b62983257c54d6c404f2b335
RX16\tamami197508 <maihei541@gmail.com>
2026-03-15 23:48:41 +0900

    [Add] pipline state struct

---
+++ Tsukino.Renderer/include/Tsukino/Renderer/DX11/PipelineState.hpp
@@ -0,0 +1,23 @@
+﻿//--------------------------------------------------------------
+//! @file   PipelineState.hpp
+//! @brief  パイプラインステート構造体の宣言
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include <d3d11.h>
+// 名前空間 : Tsukino::Renderer
+namespace Tsukino::Renderer {
+    //--------------------------------------------------------------
+    //! @struct PipelineState
+    //! @brief  パイプラインステート構造体
+    //--------------------------------------------------------------
+    struct PipelineState {
+        ID3D11VertexShader*      vs;             // 頂点シェーダー
+        ID3D11PixelShader*       ps;             // ピクセルシェーダー
+        ID3D11InputLayout*       inputLayout;    // 入力レイアウト
+        ID3D11RasterizerState*   rasterizer;     // ラスタライザーステート
+        ID3D11BlendState*        blend;          // ブレンドステート
+        ID3D11DepthStencilState* depth;          // デプスステンシルステート
+        D3D11_PRIMITIVE_TOPOLOGY topology;       // プリミティブトポロジー
+    };
+}    // namespace Tsukino::Renderer
