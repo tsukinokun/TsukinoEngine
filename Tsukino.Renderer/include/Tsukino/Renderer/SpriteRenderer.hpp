88c3707deb27cad16effc5e7a9841d773eeaebba
RX16\tamami197508 <maihei541@gmail.com>
2026-03-16 02:05:18 +0900

    [Update] sprite renderer class

--- Tsukino.Renderer/include/Tsukino/Renderer/SpriteRenderer.hpp
+++ Tsukino.Renderer/include/Tsukino/Renderer/SpriteRenderer.hpp
@@ -1,56 +1,33 @@
 ﻿//------------------------------------------------------------
 //! @file   SpriteRenderer.hpp
-//! @brief  スプライト描画クラス
+//! @brief  スプライト描画クラスの宣言
 //! @author 山﨑愛
 //------------------------------------------------------------
 #pragma once
-
-#include <Tsukino/Core/typedef.hpp>
-#include <Tsukino/Renderer/DX11/Texture/DX11Texture2D.hpp>
-
-#include <d3d11.h>
-#include <wrl/client.h>
-
 // 名前空間 : Tsukino::Renderer
 namespace Tsukino::Renderer {
-
+    // 前方宣言
+    class GraphicsContext;    
+    class Material;          
+    class MeshBuffer;
     //------------------------------------------------------------
     //! @class  SpriteRenderer
-    //! @brief  2Dスプライト描画クラス
+    //! @brief  スプライト描画クラス
     //------------------------------------------------------------
     class SpriteRenderer {
     public:
         //------------------------------------------------------------
-        // 初期化
-        //! @param device [in] DirectX11 Device
+        //! @brief デフォルトコンストラクタ
         //------------------------------------------------------------
-        bool Initialize(ID3D11Device* device);
+        SpriteRenderer() = default;
 
         //------------------------------------------------------------
-        // スプライト描画
-        //! @param context [in] DeviceContext
-        //! @param texture [in] 描画するテクスチャ
+        //! @brief sprite描画を行う関数
+        //! @param gfx       [in] グラフィックスコンテキストへのポインタ
+        //! @param material  [in] 描画に使用するマテリアルへのポインタ
+        //! @param mesh      [in] 描画に使用するメッシュデータへのポインタ
         //------------------------------------------------------------
-        void Draw(ID3D11DeviceContext* context, DX11Texture2D* texture);
-
-    private:
-        //------------------------------------------------------------
-        //! 頂点構造体
-        //------------------------------------------------------------
-        struct Vertex {
-            float position[3];
-            float uv[2];
-        };
-
-        Microsoft::WRL::ComPtr<ID3D11Buffer> m_vertexBuffer;
-        Microsoft::WRL::ComPtr<ID3D11Buffer> m_indexBuffer;
-
-        Microsoft::WRL::ComPtr<ID3D11VertexShader> m_vertexShader;
-        Microsoft::WRL::ComPtr<ID3D11PixelShader>  m_pixelShader;
-
-        Microsoft::WRL::ComPtr<ID3D11InputLayout> m_inputLayout;
-
-        Microsoft::WRL::ComPtr<ID3D11SamplerState> m_sampler;
+        void Draw(GraphicsContext* gfx, Material* material, MeshBuffer* mesh);
     };
 
 }    // namespace Tsukino::Renderer
