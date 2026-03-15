e53e27e2beb9b624b3eb3f796f5ec68b5c0a6319
RX16\tamami197508 <maihei541@gmail.com>
2026-03-16 02:46:28 +0900

    [Add] draw command clsas

---
+++ Tsukino.Renderer/include/Tsukino/Renderer/DrawCommand.hpp
@@ -0,0 +1,22 @@
+﻿//------------------------------------------------------------
+//! @file   DrawCommand.hpp
+//! @brief  描画コマンド構造体の宣言
+//! @author 山﨑愛
+//------------------------------------------------------------
+#pragma once
+#include <DirectXMath.h>
+// 名前空間 Tsukino::Renderer
+namespace Tsukino::Renderer {
+    // 前方宣言
+    class Material;
+    struct MeshBuffer;
+    //------------------------------------------------------------
+    //! @struct DrawCommand
+    //! @brief  描画コマンドを表す構造体
+    //------------------------------------------------------------
+    struct DrawCommand {
+        Material*         material;     // どう描くか（シェーダー、テクスチャ、ステート）
+        MeshBuffer*       mesh;         // 何を描くか（Quad, Cube, Model Mesh など）
+        DirectX::XMMATRIX transform;    // どこに描くか（モデル行列）
+    };
+}    // namespace Tsukino::Renderer
