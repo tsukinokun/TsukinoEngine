0df0a8ad99925fbc84c600cc76c7c87c34d92c28
RX16\tamami197508 <maihei541@gmail.com>
2026-03-20 04:51:17 +0900

    [Add] engine api class

--- Tsukino.EngineIntegration/include/Tsukino/Tsukino.EngineIntegration/EngineAPI.hpp
+++ Tsukino.EngineIntegration/include/Tsukino/Tsukino.EngineIntegration/EngineAPI.hpp
@@ -1,1 +1,29 @@
-﻿
+﻿//------------------------------------------------------------
+//! @file    EngineAPI.hpp
+//! @brief   エンジンからAPIを提供するクラスの宣言
+//! @author  山﨑愛
+//------------------------------------------------------------
+#pragma once
+#include <Tsukino/Tsukino.EngineIntegration/EngineContext.hpp>
+
+// 名前空間 : Tsukino::EngineIntegration
+namespace Tsukino::EngineIntegration {
+    struct EngineContext;    // 前方宣言
+
+    //------------------------------------------------------------
+    //! @class   EngineAPI
+    //! @brief   エンジンからAPIを提供するクラス
+    //------------------------------------------------------------
+    class EngineAPI {
+    public:
+        //------------------------------------------------------------
+        // コンストラクタ
+        //! @param   context エンジン全体で共有されるクラスのポインタを集めた構造体への参照
+        //------------------------------------------------------------
+        explicit EngineAPI(EngineContext& context);
+
+    private:
+        EngineContext& m_context;    // コンテキストへの参照
+    };
+
+}    // namespace Tsukino::EngineIntegration
