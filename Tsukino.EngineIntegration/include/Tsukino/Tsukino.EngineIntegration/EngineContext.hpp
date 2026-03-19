9b76f276cc5be762e452e855da4bb8277d544d67
RX16\tamami197508 <maihei541@gmail.com>
2026-03-20 03:30:08 +0900

    [Add] engine context struct

--- Tsukino.EngineIntegration/include/Tsukino/Tsukino.EngineIntegration/EngineContext.hpp
+++ Tsukino.EngineIntegration/include/Tsukino/Tsukino.EngineIntegration/EngineContext.hpp
@@ -1,1 +1,17 @@
-﻿
+﻿//------------------------------------------------------------
+//! @file   EngineContext.hpp
+//! @brief  エンジン全体で共有されるクラスのポインタ集
+//! @author 山﨑愛
+//------------------------------------------------------------
+#include <Tsukino/Renderer/Renderer.hpp>
+#include <Tsukino/Core/Window.hpp>
+#include <Tsukino/Engine/Asset/AssetManager.hpp>
+//------------------------------------------------------------
+//! @struct  EngineContext
+//! @brief   エンジン全体で共有されるクラスのポインタを集めた構造体
+//------------------------------------------------------------
+struct EngineContext {
+    Tsukino::Renderer::Renderer* renderer = nullptr;
+    Tsukino::Core::Window*       window   = nullptr;
+    Tsukino::Asset::AssetManager* assets = nullptr;
+};
