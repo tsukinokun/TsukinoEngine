f3c49da0d6236de39dd9788cc4813ab07e147eb5
RX16\tamami197508 <maihei541@gmail.com>
2026-03-20 04:22:40 +0900

    [Add] engine integration class

---
+++ Tsukino.EngineIntegration/include/Tsukino/Tsukino.EngineIntegration/EngineIntegration.hpp
@@ -0,0 +1,47 @@
+﻿//------------------------------------------------------------
+//! @file       EngineIntegration.hpp
+//! @brief      エンジン全体で共有されるクラスを統合したクラスの定義
+//! @author     山﨑愛
+//------------------------------------------------------------
+#include <Tsukino/Tsukino.EngineIntegration/EngineContext.hpp>
+
+#include <Tsukino/Renderer/Renderer.hpp>
+
+#include <Tsukino/Engine/Asset/AssetManager.hpp>
+
+#include <Tsukino/Core/Window.hpp>
+
+#include <memory>
+
+// 名前空間 : Tsukino::Integration
+namespace Tsukino::EngineIntegration {
+    //------------------------------------------------------------
+    //! @class   EngineIntegration
+    //! @brief   エンジン全体で共有されるクラスを統合したクラス
+    //------------------------------------------------------------
+    class EngineIntegration {
+    public:
+        //------------------------------------------------------------
+        // コンストラクタ
+        //------------------------------------------------------------
+        EngineIntegration();
+
+        //------------------------------------------------------------
+        // デストラクタ
+        //------------------------------------------------------------
+        ~EngineIntegration();
+
+        //------------------------------------------------------------
+        //! @brief  エンジン全体で共有されるクラスを初期化する関数
+        //! @return クラスのポインタを集めた構造体
+        //------------------------------------------------------------
+        EngineContext& GetContext() { return m_ctx; }
+
+    private:
+        std::unique_ptr<Tsukino::Renderer::Renderer>  m_renderer;
+        std::unique_ptr<Tsukino::Asset::AssetManager> m_assetManager;
+        std::unique_ptr<Tsukino::Core::Window>        m_window;
+
+        EngineContext m_ctx;    // エンジン全体で共有されるクラスのポインタを集めた構造体
+    };
+}    // namespace Tsukino::EngineIntegration
---
+++ Tsukino.EngineIntegration/src/EngineIntegration.cpp
@@ -0,0 +1,31 @@
+﻿//------------------------------------------------------------
+//! @file       EngineIntegration.hpp
+//! @brief      エンジン全体で共有されるクラスを統合したクラスの実装
+//! @author     山﨑愛
+//------------------------------------------------------------
+#include <Tsukino/Tsukino.EngineIntegration/EngineIntegration.hpp>
+
+#include <memory>
+
+// 名前空間 : Tsukino::Integration
+namespace Tsukino::EngineIntegration {
+    //------------------------------------------------------------
+    //! @brief コンストラクタ
+    //------------------------------------------------------------
+    EngineIntegration::EngineIntegration() {
+        //------------------------------------------------------------
+        // ユニークポインタを作成して
+        //------------------------------------------------------------
+        m_window       = std::make_unique<Tsukino::Core::Window>();
+        m_renderer     = std::make_unique<Tsukino::Renderer::Renderer>();
+        m_assetManager = std::make_unique<Tsukino::Asset::AssetManager>();
+
+        //------------------------------------------------------------
+        // コンテキストにポインタをセット
+        //------------------------------------------------------------
+        m_ctx.window   = m_window.get();
+        m_ctx.renderer = m_renderer.get();
+        m_ctx.assets   = m_assetManager.get();
+    }
+
+}    // namespace Tsukino::EngineIntegration
