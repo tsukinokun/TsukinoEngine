8bcd408a93afd24f3c8c86f4915cb53cc8b294a5
RX16\tamami197508 <maihei541@gmail.com>
2026-03-20 01:20:50 +0900

    [Add] engine integration project

---
+++ Tsukino.EngineIntegration/pch.h
@@ -0,0 +1,23 @@
+﻿//------------------------------------------------------------
+//! @file	pch.h
+//! @brief  プリコンパイル済みヘッダーファイル
+//! @note   Tsukino.EngineIntegrationプロジェクトのビルド時間を短縮用
+//! @author 山﨑愛
+//------------------------------------------------------------
+#pragma once
+
+// Windows API
+#define NOMINMAX
+#include <Windows.h>
+
+// 標準ライブラリ
+#include <string>
+#include <vector>
+#include <memory>
+#include <filesystem>
+
+// hlslpp
+#include <hlsl++.h>
+
+// entt
+#include <entt/entt.hpp>
