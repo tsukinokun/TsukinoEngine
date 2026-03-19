91f90d47e5a7a160760030840ba7afd03982a8dd
RX16\tamami197508 <maihei541@gmail.com>
2026-03-20 06:42:33 +0900

    [Add] construct api for sandbox

--- Tsukino.Sandbox/src/WinMain.cpp
+++ Tsukino.Sandbox/src/WinMain.cpp
@@ -3,14 +3,16 @@
 //! @brief	Sandboxのエントリポイント
 //! @author 山﨑愛
 //--------------------------------------------------------------
-#include <Tsukino/Renderer/Renderer.hpp>
-#include <Tsukino/Renderer/DX11/PipelineFactory.hpp>
-#include <Tsukino/Renderer/DX11/PipelineState.hpp>
+#include <Tsukino/Tsukino.EngineIntegration/EngineAPI.hpp>
+#include <Tsukino/Tsukino.EngineIntegration/EngineIntegration.hpp>
+//#include <Tsukino/Renderer/Renderer.hpp>
+//#include <Tsukino/Renderer/DX11/PipelineFactory.hpp>
+//#include <Tsukino/Renderer/DX11/PipelineState.hpp>
 #include <Tsukino/Engine/Asset/AssetManager.hpp>
-#include <Tsukino/Engine/Asset/Shader/ShaderAsset.hpp>
-#include <Tsukino/Engine/Asset/Texture/TextureAsset.hpp>
-#include <Tsukino/Core/Path.hpp>
-#include <Tsukino/Core/Window.hpp>
+//#include <Tsukino/Engine/Asset/Shader/ShaderAsset.hpp>
+//#include <Tsukino/Engine/Asset/Texture/TextureAsset.hpp>
+//#include <Tsukino/Core/Path.hpp>
+//#include <Tsukino/Core/Window.hpp>
 #include <Tsukino/Core/Log.hpp>
 
 #include <Windows.h>
@@ -23,6 +25,17 @@
 //! @return 終了コード（通常は0）
 //--------------------------------------------------------------
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
+    // ログの初期化
+    Tsukino::EngineIntegration::EngineIntegration engineIntegration;
+    // 初期化
+    if(!engineIntegration.Initialize()) {
+        // 初期化に失敗した場合はエラーログを出力して終了
+        Tsukino::Core::Log::Error("Failed to initialize EngineIntegration.");
+        return false;
+    }
+
+    Tsukino::EngineIntegration::EngineContext engineContext = engineIntegration.GetContext();
+    Tsukino::EngineIntegration::EngineAPI     engineAPI(engineContext);
 
     //--------------------------------------------------------------
     // メインループ
