f3a5ee4edf281500102e6839720df485e8dd7d8a
RX16\tamami197508 <maihei541@gmail.com>
2026-03-21 04:02:35 +0900

    [Add] built in asset class

---
+++ Tsukino.BuiltIn/include/Tsukino/BuiltIn/BuiltInAssets.hpp
@@ -0,0 +1,26 @@
+﻿//--------------------------------------------------------------
+//! @file   BuiltInAssets.hpp
+//! @brief  ビルトインアセット集約クラスの宣言
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+// 名前空間 : Tsukino::Asset
+namespace Tsukino::Asset {
+    class AssetManager;    // 前方宣言
+}
+
+// 名前空間 : Tsukino::BuiltIn
+namespace Tsukino::BuiltIn {
+    //--------------------------------------------------------------
+    //! @class   BuiltInAssets
+    //! @brief   ビルトインアセット集約クラス
+    //--------------------------------------------------------------
+    class BuiltInAssets {
+    public:
+        //--------------------------------------------------------------
+        // ビルトインアセットの初期化関数
+        //! @param assets [in] アセットマネージャーへのポインタ
+        //--------------------------------------------------------------
+        void Initialize(Tsukino::Asset::AssetManager* assets);
+    };
+}    // namespace Tsukino::BuiltIn
---
+++ Tsukino.BuiltIn/src/BuiltInAssets.cpp
@@ -0,0 +1,17 @@
+﻿//--------------------------------------------------------------
+//! @file   BuiltInAssets.cpp
+//! @brief  ビルトインアセット集約クラスの実装
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#include <Tsukino/BuiltIn/BuiltInAssets.hpp>
+
+#include <Tsukino/Engine/Asset/AssetManager.hpp>
+// 名前空間 : Tsukino::BuiltIn
+namespace Tsukino::BuiltIn {
+    //--------------------------------------------------------------
+    //! @brief ビルトインアセットの初期化関数
+    //--------------------------------------------------------------
+    void BuiltInAssets::Initialize(Tsukino::Asset::AssetManager* assets) {
+
+    }
+}    // namespace Tsukino::BuiltIn
