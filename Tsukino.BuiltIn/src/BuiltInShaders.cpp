fbad87ee1fe44e105cf9b188f4b0403c4a52c187
RX16\tamami197508 <maihei541@gmail.com>
2026-03-21 04:31:19 +0900

    [Add] built in shader class

---
+++ Tsukino.BuiltIn/src/BuiltInShaders.cpp
@@ -0,0 +1,20 @@
+﻿//--------------------------------------------------------------
+//! @file   BuiltInShaders.cpp
+//! @brief  ビルトインシェーダー集約クラスの実装
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#include <Tsukino/BuiltIn/BuiltInShaders.hpp>
+
+#include <Tsukino/Engine/Asset/AssetManager.hpp>
+
+#include <Tsukino/Core/Path.hpp>
+// 名前空間 : Tsukino::BuiltIn
+namespace Tsukino::BuiltIn {
+    //--------------------------------------------------------------
+    //! @brief ビルトインシェーダーの初期化関数
+    //--------------------------------------------------------------
+    void BuiltInShaders::Initialize(Tsukino::Asset::AssetManager* assetManager) {
+        spriteVS = assetManager->Load(Tsukino::Core::Path("BuiltIn/Shaders/SpriteVS.hlsl"));
+        spritePS = assetManager->Load(Tsukino::Core::Path("BuiltIn/Shaders/SpritePS.hlsl"));
+    }
+}    // namespace Tsukino::BuiltIn
