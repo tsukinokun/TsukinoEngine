32f24f4ca81b0b2d6993cb50d1ce1c4f06af368f
RX16\tamami197508 <maihei541@gmail.com>
2026-03-25 05:35:14 +0900

    [Add] Pipeline cache for SpriteRendererSystem

--- Tsukino.Renderer/src/DX11/PipelineFactory.cpp
+++ Tsukino.Renderer/src/DX11/PipelineFactory.cpp
@@ -14,7 +14,21 @@ namespace Tsukino::Renderer {
                                            const Tsukino::Asset::ShaderAsset& ps,
                                            const D3D11_INPUT_ELEMENT_DESC*    layout,
                                            UINT                               layoutCount) {
-        // 新しいパイプラインステートを作成
+        //--------------------------------------------------------------
+        // シェーダーのハンドル値を取り出してキーを作成
+        //--------------------------------------------------------------
+        std::pair<u64, u64> key = std::make_pair(vs.GetHandle().Value(), ps.GetHandle().Value());
+
+        //--------------------------------------------------------------
+        // キャッシュに存在する場合はそれを即座に返す（これによって毎フレームの生成コストをゼロ化）
+        //--------------------------------------------------------------
+        if(m_cache.find(key) != m_cache.end()) {
+            return m_cache[key];
+        }
+
+        //--------------------------------------------------------------
+        // 存在しない場合は新規作成
+        //--------------------------------------------------------------
         std::shared_ptr<PipelineState> p = std::make_shared<PipelineState>();
 
         // VS
@@ -26,6 +40,8 @@ namespace Tsukino::Renderer {
         // InputLayout
         m_device->CreateInputLayout(layout, layoutCount, vs.binary.data(), vs.binary.size(), p->inputLayout.GetAddressOf());
 
+        // 4. キャッシュに保存してから返す
+        m_cache[key] = p;
         return p;
     }
 
