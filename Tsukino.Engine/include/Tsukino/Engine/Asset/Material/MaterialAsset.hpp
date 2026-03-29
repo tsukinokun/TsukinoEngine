7d81160eafae7cd1268e164167c68b3dbf76336d
RX16\tamami197508 <maihei541@gmail.com>
2026-03-30 03:16:21 +0900

    [Add] MaterialAsset class

---
+++ Tsukino.Engine/include/Tsukino/Engine/Asset/Material/MaterialAsset.hpp
@@ -0,0 +1,22 @@
+﻿//--------------------------------------------------------------
+//! @file   MaterialAsset.hpp
+//! @brief  マテリアルアセットの定義
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#include <Tsukino/Engine/Asset/IAsset.hpp>
+
+#include <hlsl++.h>
+// 名前空間 ：Tsukino::Asset
+namespace Tsukino::Asset {
+    //--------------------------------------------------------------
+    //! @class  MaterialAsset
+    //! @brief  マテリアルアセットクラス
+    //--------------------------------------------------------------
+    class MaterialAsset : public IAsset {
+    public:
+        AssetHandle    textureHandle;         // テクスチャアセットのID
+        AssetHandle    vertexShaderHandle;    // バーテックスシェーダーアセットのID
+        AssetHandle    pixelShaderHandle;     // ピクセルシェーダーアセットのID
+        hlslpp::float4 diffuseColor;          // 色データ
+    };
+}    // namespace Tsukino::Asset
