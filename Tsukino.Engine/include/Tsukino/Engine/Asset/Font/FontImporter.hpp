8efbdfb204ce898e9dcb59748b94c94a2f4ea5b2
RX16\tamami197508 <maihei541@gmail.com>
2026-04-02 04:19:51 +0900

    [Add] RunProcess function for FontAsset class

--- Tsukino.Engine/include/Tsukino/Engine/Asset/Font/FontImporter.hpp
+++ Tsukino.Engine/include/Tsukino/Engine/Asset/Font/FontImporter.hpp
@@ -21,6 +21,17 @@ namespace Tsukino::Asset {
         //--------------------------------------------------------------
         [[nodiscard]]
         bool Import(const Tsukino::Core::Path& inPutPath, const Tsukino::Core::Path& outPutDirectory) override;
+
+    private:
+        //--------------------------------------------------------------
+        // 外部プロセスを実行して終了コードを返す
+        //! @param  executablePath [in] 実行ファイルの絶対パス
+        //! @param  arguments      [in] 引数文字列
+        //! @param  workingDir     [in] カレントディレクトリ
+        //! @return 実行成功かつ終了コード0なら true
+        //--------------------------------------------------------------
+        [[nodiscard]]
+        static bool RunProcess(const Tsukino::Core::Path& executablePath, const std::wstring& arguments, const Tsukino::Core::Path& workingDir);
     };
 
 }    // namespace Tsukino::Asset
