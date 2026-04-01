d7463bc1fe251a41e0110380a6489729864431c4
RX16\tamami197508 <maihei541@gmail.com>
2026-04-02 03:34:44 +0900

    [Add] FontImporter

---
+++ Tsukino.Engine/include/Tsukino/Engine/Asset/Font/FontImporter.hpp
@@ -0,0 +1,26 @@
+﻿//--------------------------------------------------------------
+//! @file	FontImporter.hpp
+//! @brief  フォントのインポータークラス
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include <Tsukino/Engine/Asset/IAssetImporter.hpp>
+// 名前空間 : Tsukino::Asset
+namespace Tsukino::Asset {
+    //--------------------------------------------------------------
+    //! @class FontImporter
+    //! @brief フォントのインポータークラス
+    //--------------------------------------------------------------
+    class FontImporter : public IAssetImporter {
+    public:
+        //--------------------------------------------------------------
+        //! @brief  フォントアセットをインポートする関数
+        //! @param  path            [in] インポートするフォントアセットのパス
+        //! @param  outPutDirectory [in] 出力先ディレクトリ
+        //! @return インポートされたフォントアセットの参照
+        //--------------------------------------------------------------
+        [[nodiscard]]
+        bool Import(const Tsukino::Core::Path& inPutPath, const Tsukino::Core::Path& outPutDirectory) override;
+    };
+
+}    // namespace Tsukino::Asset
