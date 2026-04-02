29564caa1159b97d89e29328c195b233f4e2a79c
RX16\tamami197508 <maihei541@gmail.com>
2026-04-02 16:58:25 +0900

    [Add] CustomDraw for Renderer class

--- Tsukino.Renderer/include/Tsukino/Renderer/DrawCommand.hpp
+++ Tsukino.Renderer/include/Tsukino/Renderer/DrawCommand.hpp
@@ -15,8 +15,9 @@ namespace Tsukino::Renderer {
     //! @brief  描画コマンドを表す構造体
     //------------------------------------------------------------
     struct DrawCommand {
-        Material*         material;     // どう描くか（シェーダー、テクスチャ、ステート）
-        MeshBuffer*       mesh;         // 何を描くか（Quad, Cube, Model Mesh など）
-        Tsukino::Core::Math::matrix transform;    // どこに描くか（モデル行列）
+        Material*                                 material;      // どう描くか（シェーダー、テクスチャ、ステート）
+        MeshBuffer*                               mesh;          // 何を描くか（Quad, Cube, Model Mesh など）
+        std::function<void(ID3D11DeviceContext*)> customDraw;    // カスタム描画関数（nullptr なら通常描画）
+        Tsukino::Core::Math::matrix               transform;     // どこに描くか（モデル行列）
     };
 }    // namespace Tsukino::Renderer
