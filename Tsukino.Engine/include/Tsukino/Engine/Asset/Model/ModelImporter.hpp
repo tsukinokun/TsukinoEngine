3a9ad54534f74012ad596c95cbd30238764973fa
GALLERIA\maihe <maihei541@gmail.com>
2026-04-11 15:37:44 +0900

    [Add] ModelImporterClass

---
+++ Tsukino.Engine/include/Tsukino/Engine/Asset/Model/ModelImporter.hpp
@@ -0,0 +1,26 @@
+﻿//--------------------------------------------------------------
+//! @file	ModelImporter.hpp
+//! @brief  モデルのインポータークラス
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include <Tsukino/Engine/Asset/IAssetImporter.hpp>
+// 名前空間 : Tsukino::Asset
+namespace Tsukino::Asset {
+    //--------------------------------------------------------------
+    //! @class ModelImporter
+    //! @brief モデルのインポータークラス
+    //--------------------------------------------------------------
+    class ModelImporter : public IAssetImporter {
+    public:
+        //--------------------------------------------------------------
+        //! @brief  モデルアセットをインポートする関数
+        //! @param  path            [in] インポートするモデルアセットのパス
+        //! @param  outPutDirectory [in] 出力先ディレクトリ
+        //! @return インポートされたモデルアセットの参照
+        //--------------------------------------------------------------
+        [[nodiscard]]
+        bool Import(const Tsukino::Core::Path& inPutPath, const Tsukino::Core::Path& outPutDirectory) override;
+    };
+
+}    // namespace Tsukino::Asset
