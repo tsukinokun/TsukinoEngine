23029b6283722edd31eb3d3c14be1f3601e93b8e
GALLERIA\maihe <maihei541@gmail.com>
2026-04-23 22:19:26 +0900

    [Fix] ModelData In ModelAsset

--- Tsukino.Engine/include/Tsukino/Engine/Asset/Model/ModelAsset.hpp
+++ Tsukino.Engine/include/Tsukino/Engine/Asset/Model/ModelAsset.hpp
@@ -6,35 +6,14 @@
 #pragma once
 #include <Tsukino/Engine/Asset/IAsset.hpp>
 
+#include <Tsukino/GraphicsCommon/Model/ModelData.hpp>
+
 #include <Tsukino/Core/Math/Matrix.hpp>
 
 #include <vector>
 #include <string>
 // 名前空間 ：Tsukino::Asset
 namespace Tsukino::Asset {
-    //--------------------------------------------------------------
-    //! @struct RenderUnit
-    //! @brief  描画に必要なメッシュとマテリアルの組み合わせを表す構造体
-    //--------------------------------------------------------------
-    struct RenderUnit {
-        AssetHandle meshHandle;        // MeshAssetへのID
-        AssetHandle materialHandle;    // MaterialAssetへのID
-    };
-
-    //--------------------------------------------------------------
-    //! @struct ModelNode
-    //! @brief  モデルの階層構造を表すノード
-    //--------------------------------------------------------------
-    struct ModelNode {
-        std::string                 name;         // ノード名（"Arm_L" など）
-        Tsukino::Core::Math::matrix transform;    // 親ノードからの相対座標（Local Transform）
-
-        std::vector<RenderUnit> renderUnits;    // このノードが持つ描画パーツ
-
-        int              parentIndex = -1;    // 親ノードのインデックス
-        std::vector<int> children;            // 子ノードのインデックス
-    };
-
     //--------------------------------------------------------------
     //! @class  ModelAsset
     //! @brief  複数のメッシュとマテリアルを統括するアセット
@@ -64,14 +43,8 @@ namespace Tsukino::Asset {
         //--------------------------------------------------------------
         void SetHandle(const AssetHandle& h) { m_handle = h; }
 
-        // 全ノードのリスト（フラットに持っておくと更新処理が速い）
-        std::vector<ModelNode> nodes;
-
-        // 全体の境界ボックス（カリング用）
-        // BoundingBox aabb;
-
-        // アニメーションデータ（将来的にここへ追加）
-        // std::vector<AnimationData> animations;
+        // モデルデータ
+        Tsukino::GraphicsCommon::ModelData modelData;
 
     private:
         AssetHandle m_handle;    // アセットのハンドル
