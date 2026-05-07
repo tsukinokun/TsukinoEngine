26a596ea21caaf6fb798c375c11b57663bd8ea35
GALLERIA\maihe <maihei541@gmail.com>
2026-05-07 19:50:05 +0900

    [Add] BlockBreaking SampleScene class

---
+++ Tsukino.Sandbox/src/Scene/BlockBreakinigSampleScene.cpp
@@ -0,0 +1,35 @@
+﻿//-------------------------------------------------------------
+//! @file    BlockBreakingSampleScene.cpp
+//! @brief   サンプルシーン1の実装
+//! @author  山﨑愛
+//-------------------------------------------------------------
+#include <Tsukino/Sandbox/Scene/BlockBreakingSampleScene.hpp>
+
+
+#include <entt/entt.hpp>
+#include <hlsl++.h>
+// 名前空間 : Tsukino::Sandbox
+namespace Tsukino::Sandbox {
+    //-------------------------------------------------------------
+    //! @brief  シーン固有の初期化処理
+    //-------------------------------------------------------------
+    void BlockBreakingSampleScene::OnInitialize(Tsukino::EngineIntegration::EngineAPI& api) {
+        // コンテキストをレジストリから取得
+        Tsukino::EngineIntegration::EngineContext* context = m_scene.GetRegistry().GetContext<Tsukino::EngineIntegration::EngineContext*>();
+    }
+
+    //-------------------------------------------------------------
+    //! @brief  シーンの更新
+    //-------------------------------------------------------------
+    void BlockBreakingSampleScene::OnUpdate(Tsukino::EngineIntegration::EngineAPI& api, float deltaTime) {
+        m_scene.Update(deltaTime);
+    }
+
+    //-------------------------------------------------------------
+    //! @brief  シーンの終了処理
+    //-------------------------------------------------------------
+    void BlockBreakingSampleScene::OnExit() {
+        // シーン終了時の解放処理などが必要な場合はここに記述します
+    }
+
+}    // namespace Tsukino::Sandbox
