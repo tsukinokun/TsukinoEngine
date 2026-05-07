c8286bb0089f8d8926df82d8865339444e716bdb
GALLERIA\maihe <maihei541@gmail.com>
2026-05-07 19:25:33 +0900

    [Add] RigidbodyComponent

--- Tsukino.BuiltIn/include/Tsukino/BuiltIn/ECS/Component/RigidbodyComponent.hpp
+++ Tsukino.BuiltIn/include/Tsukino/BuiltIn/ECS/Component/RigidbodyComponent.hpp
@@ -1,1 +1,37 @@
-﻿
+﻿//-------------------------------------------------------------
+//! @file   RigidbodyComponent.hpp
+//! @brief  RigidbodyComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <hlsl++.h>
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+
+    //-------------------------------------------------------------
+    //! @enum class RigidbodyType
+    //! @brief  剛体の種類を定義する列挙型
+    //-------------------------------------------------------------
+    enum class RigidbodyType {
+        Static,       // 動かない（床、壁）
+        Kinematic,    // プログラムから座標を直接制御する（動く床、アニメーションする扉）
+        Dynamic       // 物理シミュレーションに従う（落下、衝突反発）
+    };
+
+    //-------------------------------------------------------------
+    //! @struct RigidbodyComponent
+    //-------------------------------------------------------------
+    struct RigidbodyComponent {
+        RigidbodyType type = RigidbodyType::Dynamic;
+
+        float mass        = 1.0f;    // 質量
+        float friction    = 0.5f;    // 摩擦係数
+        float restitution = 0.0f;    // 反発係数
+
+        hlslpp::float3 linearVelocity  = {0, 0, 0};    // 線速度
+        hlslpp::float3 angularVelocity = {0, 0, 0};    // 角速度
+
+        // Jolt内部でボディを生成するためのトリガー
+        bool isInitialized = false;
+    };
+}    // namespace Tsukino::BuiltIn::ECS
