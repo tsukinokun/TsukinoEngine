53ede7895dd9c5fd1dfbc3eba58b5bf3ad420e45
DESKTOP-RAM23UQ\cre <a@a>
2026-05-08 15:00:23 +0900

    [Add] WallComponent

---
+++ Tsukino.Sandbox/include/Tsukino/Sandbox/BlockBreakingSample/ECS/Component/WallComponent.hpp
@@ -0,0 +1,15 @@
+﻿//-------------------------------------------------------------
+//! @file   WallComponent.hpp
+//! @brief  WallComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <Tsukino/Core/Math/Matrix.hpp>
+// 名前空間 : BlockBreakingSample::ECS
+namespace BlockBreakingSample::ECS {
+    //-------------------------------------------------------------
+    //! @struct WallComponent
+    //! @brief  壁に必要なCmoponent
+    //-------------------------------------------------------------
+    struct WallComponent {};
+}    // namespace BlockBreakingSample::ECS
