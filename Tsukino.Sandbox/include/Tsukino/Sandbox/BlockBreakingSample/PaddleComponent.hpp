8c5a688d14e76e919e85489d24f30fa737fcf770
DESKTOP-RAM23UQ\cre <a@a>
2026-05-08 12:20:01 +0900

    [Add] PaddleComponent

---
+++ Tsukino.Sandbox/include/Tsukino/Sandbox/BlockBreakingSample/PaddleComponent.hpp
@@ -0,0 +1,15 @@
+﻿//-------------------------------------------------------------
+//! @file   PaddleComponent.hpp
+//! @brief  PaddleComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <Tsukino/Core/Math/Matrix.hpp>
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+    //-------------------------------------------------------------
+    //! @struct PaddleComponent
+    //! @brief  パドルに必要なCmoponent
+    //-------------------------------------------------------------
+    struct PaddleComponent {};
+}    // namespace Tsukino::BuiltIn::ECS
