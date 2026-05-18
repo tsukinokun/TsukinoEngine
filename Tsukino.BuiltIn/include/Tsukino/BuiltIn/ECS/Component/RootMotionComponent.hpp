abd04c501c1e8cf0bbe0822b7133814972052586
607-03\cre <maihei541@gmail.com>
2026-05-18 14:02:15 +0900

    [Add] AnimationComponents

---
+++ Tsukino.BuiltIn/include/Tsukino/BuiltIn/ECS/Component/RootMotionComponent.hpp
@@ -0,0 +1,18 @@
+﻿//-------------------------------------------------------------
+//! @file   RootMotionComponent.hpp
+//! @brief  RootMotionComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <hlsl++.h>
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+    //-------------------------------------------------------------
+    //! @class  RootMotionComponent
+    //! @brief  ルートモーションの移動量と回転量を管理するコンポーネント
+    //-------------------------------------------------------------
+    struct RootMotionComponent {
+        hlslpp::float3     delta_position = hlslpp::float3(0.0f, 0.0f, 0.0f);    // 前フレームからの移動量
+        hlslpp::quaternion delta_rotation = hlslpp::quaternion(0.0f, 0.0f, 0.0f, 1.0f);    // 前フレームからの回転量
+    };
+}    // namespace Tsukino::BuiltIn::ECS
---
+++ Tsukino.BuiltIn/include/Tsukino/BuiltIn/ECS/Component/SkeletonOutputComponent.hpp
@@ -0,0 +1,20 @@
+﻿//-------------------------------------------------------------
+//! @file   SkeletonOutputComponent.hpp
+//! @brief  SkeletonOutputComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <Tsukino/Core/typedef.hpp>
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+    //-------------------------------------------------------------
+    //! @class  SkeletonOutputComponent
+    //! @brief  スケルトンの出力を管理するコンポーネント
+    //-------------------------------------------------------------
+    struct SkeletonOutputComponent {
+        static constexpr int MAX_BONES = 64;                // 最大ボーン数
+        alignas(16) float local_matrices[MAX_BONES][16];    // 各ボーンのローカル行列
+        u32 bone_count;                                     // 実際のボーン数
+    };
+
+}    // namespace Tsukino::BuiltIn::ECS
