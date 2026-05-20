d2e44173af530ed500418c7483e7e0a8f1a2cda7
502-32\cre <a>
2026-05-20 16:12:18 +0900

    Revert "[WIP]"
    This reverts commit 8bb3af073f24b908270b0b950ab5d3b666806ee0.

--- Tsukino.Sandbox/src/WinMain.cpp
+++ Tsukino.Sandbox/src/WinMain.cpp
@@ -36,8 +36,8 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
     //--------------------------------------------------------------
     // 最初のシーンを登録・開始
     //--------------------------------------------------------------
-    engineAPI.ChangeScene(std::make_unique<Tsukino::Sandbox::SampleScene1>());
-    //engineAPI.ChangeScene(std::make_unique<Tsukino::Sandbox::BlockBreakingSampleScene>());
+    //engineAPI.ChangeScene(std::make_unique<Tsukino::Sandbox::SampleScene1>());
+    engineAPI.ChangeScene(std::make_unique<Tsukino::Sandbox::BlockBreakingSampleScene>());
 
     //--------------------------------------------------------------
     // メインループ
