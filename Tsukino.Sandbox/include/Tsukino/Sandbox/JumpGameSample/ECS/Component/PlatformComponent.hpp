619cb02222715c74d726b6780c7a40549c34ee72
GALLERIA\maihe <maihei541@gmail.com>
2026-06-03 04:33:24 +0900

    [Add] PlatformComponent

---
+++ Tsukino.Sandbox/include/Tsukino/Sandbox/JumpGameSample/ECS/Component/PlatformComponent.hpp
@@ -0,0 +1,18 @@
+﻿//-------------------------------------------------------------
+//! @file   PlatformComponent.hpp
+//! @brief  PlatformComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <Tsukino/Core/Math/Matrix.hpp>
+// 名前空間 : JumpGameSample::ECS
+namespace JumpGameSample::ECS {
+    //-------------------------------------------------------------
+    //! @struct PlatformComponent
+    //! @brief  土台に必要なComponent
+    //-------------------------------------------------------------
+    struct PlatformComponent {
+        float speed    = 50.0f;
+        bool  isMoving = true;
+    };
+}    // namespace JumpGameSample::ECS
