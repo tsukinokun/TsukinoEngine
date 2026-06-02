e97355c027ebd7018bf0fc05781856d9bfba0a05
GALLERIA\maihe <maihei541@gmail.com>
2026-06-02 23:38:12 +0900

    [Add] ImpulseRequestComponent

---
+++ Tsukino.BuiltIn/include/Tsukino/BuiltIn/ECS/Component/ImpulseRequestComponent.hpp
@@ -0,0 +1,20 @@
+﻿//-------------------------------------------------------------
+//! @file   ImpulseRequestComponent.hpp
+//! @brief  ImpulseRequestComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <Tsukino/Core/ECS/Entity/Entity.hpp>
+
+#include <hlsl++.h>
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+    //-------------------------------------------------------------
+    //! @class  ImpulseRequestComponent
+    //! @brief  物理エンジンへの衝撃要求を表すコンポーネント
+    //-------------------------------------------------------------
+    struct ImpulseRequestComponent {
+        hlslpp::float3 impulse;    //!< 衝撃のベクトル (方向と大きさ)
+    };
+
+}    // namespace Tsukino::BuiltIn::ECS
