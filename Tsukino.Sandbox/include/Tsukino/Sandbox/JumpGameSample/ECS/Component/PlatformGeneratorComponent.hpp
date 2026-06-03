21241e599ad35e3d38122aca1e6504440cf8941e
GALLERIA\maihe <maihei541@gmail.com>
2026-06-03 20:25:46 +0900

    [Add] PlatformGeneratorComponent

---
+++ Tsukino.Sandbox/include/Tsukino/Sandbox/JumpGameSample/ECS/Component/PlatformGeneratorComponent.hpp
@@ -0,0 +1,18 @@
+﻿//-------------------------------------------------------------
+//! @file   PlatformGeneratorComponent.hpp
+//! @brief  PlatformGeneratorComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <Tsukino/Core/Math/Matrix.hpp>
+// 名前空間 : JumpGameSample::ECS
+namespace JumpGameSample::ECS {
+    //-------------------------------------------------------------
+    //! @struct PlatformGeneratorComponent
+    //! @brief  土台生成器に必要なComponent
+    //-------------------------------------------------------------
+    struct PlatformGeneratorComponent {
+        bool  canSpawn      = true;    // 次を生成して良いか
+        float spawnDistance = 5.0f;    // 次の土台が出るまでの距離やオフセットなど
+    };
+}    // namespace JumpGameSample::ECS
