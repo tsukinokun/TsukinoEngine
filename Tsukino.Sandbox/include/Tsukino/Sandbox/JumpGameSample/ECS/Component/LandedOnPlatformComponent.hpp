748a12b4ccaa365c1c2ee3cc419cd21f7c186d08
502-32\cre <a>
2026-06-03 15:15:44 +0900

    [Add] LandedOnPlatformComponent

---
+++ Tsukino.Sandbox/include/Tsukino/Sandbox/JumpGameSample/ECS/Component/LandedOnPlatformComponent.hpp
@@ -0,0 +1,19 @@
+﻿//-------------------------------------------------------------
+//! @file   LandedOnPlatformComponent.hpp
+//! @brief  LandedOnPlatformComponentクラスの宣言
+//! @brief  LandedOnPlatformComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <Tsukino/Core/ECS/Entity/Entity.hpp>
+// 名前空間 : JumpGameSample::ECS
+namespace JumpGameSample::ECS {
+    //-------------------------------------------------------------
+    //! @struct LandedOnPlatformComponent
+    //! @brief  どの土台に乗っているかを示すComponent
+    //-------------------------------------------------------------
+    struct LandedOnPlatformComponent {
+        Tsukino::ECS::Entity platformEntity = entt::null;
+    };
+
+}    // namespace JumpGameSample::ECS
