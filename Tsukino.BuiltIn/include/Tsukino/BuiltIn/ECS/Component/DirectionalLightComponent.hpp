7089eab16ecfb9d9728dce1eb675a14d0e9e1aba
GALLERIA\maihe <maihei541@gmail.com>
2026-06-06 12:24:27 +0900

    [Add] DirectionalLightComponent

---
+++ Tsukino.BuiltIn/include/Tsukino/BuiltIn/ECS/Component/DirectionalLightComponent.hpp
@@ -0,0 +1,22 @@
+﻿//--------------------------------------------------------------
+//! @file   DirectionalLightComponent.hpp
+//! @brief  ディレクショナルライトコンポーネント
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#pragma once
+#include <hlsl++.h>
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+    //--------------------------------------------------------------
+    //! @struct DirectionalLightComponent
+    //! @brief  平行光源の設定を持つコンポーネント
+    //! @note   エンティティにアタッチして使用する
+    //!         シーン内に複数存在する場合は最初に見つかったものを使用する
+    //--------------------------------------------------------------
+    struct DirectionalLightComponent {
+        hlslpp::float3 direction  = {0.0f, -1.0f, 0.0f};    //!< ライトの方向（正規化推奨）
+        hlslpp::float3 color      = {1.0f, 1.0f, 1.0f};     //!< ライトの色
+        float          intensity  = 1.0f;                   //!< ライトの強度
+        bool           castShadow = true;                   //!< シャドウを落とすか
+    };
+}    // namespace Tsukino::BuiltIn::ECS
