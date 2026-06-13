d14f167edd29915b1c81c6b214045604e4a4748c
GALLERIA\maihe <maihei541@gmail.com>
2026-06-13 15:05:58 +0900

    [Add] DebugCameraSystem

---
+++ Tsukino.BuiltIn/include/Tsukino/BuiltIn/ECS/Component/DebugCameraTag.hpp
@@ -0,0 +1,16 @@
+﻿//-------------------------------------------------------------
+//! @file   DebugCameraTag.hpp
+//! @brief  DebugCameraTagクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+    //-------------------------------------------------------------
+    //! @class  DebugCameraTag
+    //! @brief  デバッグカメラ用のタグコンポーネント
+    //-------------------------------------------------------------
+    struct DebugCameraTag {
+        bool dummy = true;
+    };
+}    // namespace Tsukino::BuiltIn::ECS
---
+++ Tsukino.EngineIntegration/include/Tsukino/EngineIntegration/ECS/System/DebugCameraSystem.hpp
@@ -0,0 +1,26 @@
+﻿//-------------------------------------------------------------
+//! @file   DebugCameraSystem.hpp
+//! @brief  DebugCameraSystemクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#ifdef _DEBUG
+#include <Tsukino/Core/ECS/System/ISystem.hpp>
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+    //-------------------------------------------------------------
+    //! @class  DebugCameraSystem
+    //! @brief  デバッグカメラの操作を管理するシステム
+    //-------------------------------------------------------------
+    class DebugCameraSystem final : public Tsukino::ECS::ISystem {
+    public:
+        //-------------------------------------------------------------
+        // システムの更新
+        //! @param  registry    [in] ECS レジストリ
+        //! @param  deltaTime   [in] 前フレームからの経過時間
+        //-------------------------------------------------------------
+        void Update(Tsukino::ECS::Registry& registry, float deltaTime) override;
+    };
+
+}    // namespace Tsukino::BuiltIn::ECS
+#endif    // _DEBUG
