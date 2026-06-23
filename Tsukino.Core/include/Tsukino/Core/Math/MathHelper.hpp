a481cf05946132928100cedcd320025c0662d493
GALLERIA\maihe <maihei541@gmail.com>
2026-06-24 04:03:30 +0900

    [Add] Intersects In Math

--- Tsukino.Core/include/Tsukino/Core/Math/MathHelper.hpp
+++ Tsukino.Core/include/Tsukino/Core/Math/MathHelper.hpp
@@ -16,14 +16,29 @@ namespace Tsukino::Core::Math {
     //--------------------------------------------------------------
     //! @brief 度数法を弧度法に変換する関数
     //--------------------------------------------------------------
-    inline float ToRadians(float degrees) {
+    inline [[nodiscard]]
+    float ToRadians(float degrees) {
         return degrees * Deg2Rad;
     }
 
     //--------------------------------------------------------------
     //! @brief 弧度法を度数法に変換する関数
     //--------------------------------------------------------------
-    inline float ToDegrees(float radians) {
+    inline [[nodiscard]]
+    float ToDegrees(float radians) {
         return radians * Rad2Deg;
     }
-}    // namespace Tsukino::Math
+
+    //--------------------------------------------------------------
+    //! @brief 2つの矩形（AABB）が重なっているかを判定する
+    //! @param [in] min1 矩形1の左上座標
+    //! @param [in] size1 矩形1のサイズ
+    //! @param [in] min2 矩形2の左上座標
+    //! @param [in] size2 矩形2のサイズ
+    //! @return 重なっている場合はtrue、そうでない場合はfalse
+    //--------------------------------------------------------------
+    inline [[nodiscard]]
+    bool Intersects(const hlslpp::float2& min1, const hlslpp::float2& size1, const hlslpp::float2& min2, const hlslpp::float2& size2) {
+        return (min1.x < min2.x + size2.x) && (min1.x + size1.x > min2.x) && (min1.y < min2.y + size2.y) && (min1.y + size1.y > min2.y);
+    }
+}    // namespace Tsukino::Core::Math
