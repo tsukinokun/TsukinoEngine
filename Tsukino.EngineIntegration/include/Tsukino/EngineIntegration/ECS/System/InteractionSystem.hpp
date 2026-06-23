ae157619f309fd6ffd190d66eca77a7fc6667480
GALLERIA\maihe <maihei541@gmail.com>
2026-06-24 04:50:25 +0900

    [Add] InteractionSystem class

---
+++ Tsukino.EngineIntegration/include/Tsukino/EngineIntegration/ECS/System/InteractionSystem.hpp
@@ -0,0 +1,26 @@
+﻿//-------------------------------------------------------------
+//! @file   InteractionSystem.hpp
+//! @brief  InteractionSystemクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <Tsukino/Core/ECS/System/ISystem.hpp>
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+    struct TransformComponent;    // 前方宣言
+
+    //-------------------------------------------------------------
+    //! @class  InteractionSystem
+    //! @brief  インタラクションを管理するシステム
+    //-------------------------------------------------------------
+    class InteractionSystem final : public Tsukino::ECS::ISystem {
+    public:
+        //-------------------------------------------------------------
+        // システムの更新
+        //! @param  registry    [in] ECS レジストリ
+        //! @param  deltaTime   [in] 前フレームからの経過時間
+        //-------------------------------------------------------------
+        void Update(Tsukino::ECS::Registry& registry, float deltaTime) override;
+    };
+
+}    // namespace Tsukino::BuiltIn::ECS
