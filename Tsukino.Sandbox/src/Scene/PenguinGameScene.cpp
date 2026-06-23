099aa5df0dffbaa0341f369754cc79abec8987b8
GALLERIA\maihe <maihei541@gmail.com>
2026-06-24 01:09:05 +0900

    [Add] PenguinAsset

--- Tsukino.Sandbox/src/Scene/PenguinGameScene.cpp
+++ Tsukino.Sandbox/src/Scene/PenguinGameScene.cpp
@@ -11,6 +11,16 @@
 #include <Tsukino/Engine/ECS/Prefab/PrefabFactory.hpp>
 #include <Tsukino/EngineIntegration/Scene/GameSceneManager.hpp>
 
+#include <Tsukino/BuiltIn/ECS/Component/TransformComponent.hpp>
+#include <Tsukino/BuiltIn/ECS/Component/CameraComponent.hpp>
+#include <Tsukino/BuiltIn/ECS/Component/SpriteComponent.hpp>
+#include <Tsukino/BuiltIn/ECS/Component/FontComponent.hpp>
+
+#include <Tsukino/EngineIntegration/ECS/System/TransformSystem.hpp>
+#include <Tsukino/EngineIntegration/ECS/System/CameraSystem.hpp>
+#include <Tsukino/EngineIntegration/ECS/System/SpriteRendererSystem.hpp>
+#include <Tsukino/EngineIntegration/ECS/System/FontRendererSystem.hpp>
+
 #include <Tsukino/Core/Path.hpp>
 #include <Tsukino/Core/Log.hpp>
 #include <Tsukino/Core/Input/InputSystem.hpp>
@@ -33,6 +43,26 @@ namespace Tsukino::Sandbox {
         // クリアカラーを透明に設定
         //--------------------------------------------------------------
         context->renderer->SetClearColor(0.0f, 0.0f, 0.0f, 0.0f);
+
+        //--------------------------------------------------------------
+        // システムの生成と追加
+        //--------------------------------------------------------------
+        // enumを使って優先度を管理
+        enum class SystemPriority : int {
+            Transform = 0,
+            Camera    = 5,
+            Font      = 9,
+            Sprite    = 10
+        };
+
+        // Transformは一番最初に計算する 
+        m_scene.AddSystem(std::make_shared<Tsukino::BuiltIn::ECS::TransformSystem>(), static_cast<int>(SystemPriority::Transform));
+        // カメラは描画前に更新する
+        m_scene.AddSystem(std::make_shared<Tsukino::BuiltIn::ECS::CameraSystem>(), static_cast<int>(SystemPriority::Camera));
+        // フォント描画
+        m_scene.AddSystem(std::make_shared<Tsukino::BuiltIn::ECS::FontRendererSystem>(), static_cast<int>(SystemPriority::Font));
+        // スプライトなど描画用のコマンド生成は後で行う
+        m_scene.AddSystem(std::make_shared<Tsukino::BuiltIn::ECS::SpriteRenderSystem>(), static_cast<int>(SystemPriority::Sprite));
     }
 
     void PenguinGameScene::OnUpdate(Tsukino::EngineIntegration::EngineAPI& api, float deltaTime) {
