ac3def7647fda644c227f03b03f7426c200c9b4d
GALLERIA\maihe <maihei541@gmail.com>
2026-06-24 04:10:22 +0900

    [Add] BoxCollider2DComponent

---
+++ Tsukino.BuiltIn/include/Tsukino/BuiltIn/ECS/Component/BoxCollider2DComponent.hpp
@@ -0,0 +1,19 @@
+﻿//-------------------------------------------------------------
+//! @file   BoxCollider2DComponent.hpp
+//! @brief  BoxCollider2DComponentクラスの宣言
+//! @author 山﨑愛
+//-------------------------------------------------------------
+#pragma once
+#include <hlsl++.h>
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+    //-------------------------------------------------------------
+    //! @class  BoxCollider2DComponent
+    //! @brief  2Dボックスコリジョンコンポーネント
+    //-------------------------------------------------------------
+    struct BoxCollider2DComponent {
+        hlslpp::float2 offset;
+        hlslpp::float2 size;
+    };
+
+}    // namespace Tsukino::BuiltIn::ECS
