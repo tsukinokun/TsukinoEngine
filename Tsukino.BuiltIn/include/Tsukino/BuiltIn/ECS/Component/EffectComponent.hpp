5c808db045aa451db6b319ec9e4f067a81328cf7
503-04\cre <maihei541@gmail.com>
2026-07-23 11:44:47 +0900

    [Add] EffectComponent

---
+++ Tsukino.BuiltIn/include/Tsukino/BuiltIn/ECS/Component/EffectComponent.hpp
@@ -0,0 +1,24 @@
+#pragma once
+//--------------------------------------------------------------
+//! @file   EffectComponent.hpp
+//! @brief  エフェクトコンポーネントの定義
+//! @author 山﨑愛
+//--------------------------------------------------------------
+#include <Tsukino/Engine/Asset/AssetHandle.hpp>
+
+// 名前空間 : Tsukino::BuiltIn::ECS
+namespace Tsukino::BuiltIn::ECS {
+
+    //--------------------------------------------------------------
+    //! @struct  EffectComponent
+    //! @brief  Effekseerエフェクトを再生するためのコンポーネント
+    //--------------------------------------------------------------
+    struct EffectComponent {
+        Tsukino::Asset::AssetHandle effectAsset;   //!< .efk アセット
+        int                         handle = -1;   //!< Effekseer インスタンスハンドル
+        float                       playSpeed = 1.0f;  //!< 再生速度
+        bool                        looping = false;    //!< ループ再生フラグ
+        bool                        stopped = false;    //!< 停止フラグ
+        bool                        active = false;     //!< 再生中フラグ
+    };
+}
